948193c9fca598164e5ff1224c906cf64af3dfd2
Prince kumar <princekr3006@gmail.com>
2025-09-15 18:37:10 +0530

    fixeed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path' 
 
 export default defineConfig({
   plugins: [
@@ -8,4 +9,9 @@ export default defineConfig({
     tailwindcss(),
   ],
   base: "/",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
